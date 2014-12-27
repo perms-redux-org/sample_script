fad10e238eede31548c7b9b600ad19aac02af265
Ed Sweeney <ed@onextent.com>
2014-12-27 10:59:29 -0800

    tagged publish

--- .travis.istagged.sh
+++ .travis.istagged.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-`bash --version`
-
 RC=-1
-if [[ `git describe --exact-match HEAD` == v* ]]; then RC=0; else RC=1; fi
+#if [[ `git describe --exact-match HEAD` == v* ]]; then RC=0; else RC=1; fi
+if [[ 1 ]]; then RC=0; else RC=1; fi
 
 echo "check tag rc: $RC"
 
