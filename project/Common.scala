503afe9e2e3090969ee04fdf3ce3dc73ed71b9f1
Ed Sweeney <ed@onextent.com>
2014-12-27 17:56:42 -0800

    clean up Commons

--- project/Common.scala
+++ project/Common.scala
@@ -5,7 +5,6 @@ object Common {
     def organization = "onextent"
     def repo_org = "navicore"
     def buildNum = sys.env.getOrElse("TRAVIS_BUILD_NUMBER", default = "0")
-    //def version = s"0.1.$buildNum"
     def scalaVersion = "2.11.4"
     def license = ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0"))
 }
